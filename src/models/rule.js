ac0345a1abbb5d0d1bd9030a3b426c3b836c105d
Lcoder <li-tao@hotmail.com>
2018-08-03 10:05:17 +0800

    inital data

---
+++ src/models/rule.js
@@ -0,0 +1,47 @@
+import { queryRule, removeRule, addRule } from '../services/api';
+
+export default {
+  namespace: 'rule',
+
+  state: {
+    data: {
+      list: [],
+      pagination: {},
+    },
+  },
+
+  effects: {
+    *fetch({ payload }, { call, put }) {
+      const response = yield call(queryRule, payload);
+      yield put({
+        type: 'save',
+        payload: response,
+      });
+    },
+    *add({ payload, callback }, { call, put }) {
+      const response = yield call(addRule, payload);          // call 是指用异步的方式来调用函数
+      yield put({
+        type: 'save',
+        payload: response,
+      });
+      if (callback) callback();
+    },
+    *remove({ payload, callback }, { call, put }) {
+      const response = yield call(removeRule, payload);
+      yield put({
+        type: 'save',
+        payload: response,
+      });
+      if (callback) callback();
+    },
+  },
+
+  reducers: {
+    save(state, action) {
+      return {
+        ...state,
+        data: action.payload,
+      };
+    },
+  },
+};
---
+++ src/routes/Dashboard/Workplace.js
@@ -0,0 +1,277 @@
+import React, { PureComponent } from 'react';
+import moment from 'moment';
+import { connect } from 'dva';
+import { Link } from 'dva/router';
+import { Row, Col, Card, List, Avatar } from 'antd';
+
+import { Radar } from 'components/Charts';
+import EditableLinkGroup from 'components/EditableLinkGroup';
+import PageHeaderLayout from '../../layouts/PageHeaderLayout';
+import aler from "../../assets/ts-alert-shine.svg" 
+
+import styles from './Workplace.less';
+
+const links = [
+  {
+    title: '操作一',
+    href: '',
+  },
+  {
+    title: '操作二',
+    href: '',
+  },
+  {
+    title: '操作三',
+    href: '',
+  },
+  {
+    title: '操作四',
+    href: '',
+  },
+  {
+    title: '操作五',
+    href: '',
+  },
+  {
+    title: '操作六',
+    href: '',
+  },
+];
+
+const members = [
+  {
+    id: 'members-1',
+    title: '科学搬砖组',
+    logo: 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png',
+    link: '',
+  },
+  {
+    id: 'members-2',
+    title: '程序员日常',
+    logo: 'https://gw.alipayobjects.com/zos/rmsportal/cnrhVkzwxjPwAaCfPbdc.png',
+    link: '',
+  },
+  {
+    id: 'members-3',
+    title: '设计天团',
+    logo: 'https://gw.alipayobjects.com/zos/rmsportal/gaOngJwsRYRaVAuXXcmB.png',
+    link: '',
+  },
+  {
+    id: 'members-4',
+    title: '中二少女团',
+    logo: 'https://gw.alipayobjects.com/zos/rmsportal/ubnKSIfAJTxIgXOKlciN.png',
+    link: '',
+  },
+  {
+    id: 'members-5',
+    title: '骗你学计算机',
+    logo: 'https://gw.alipayobjects.com/zos/rmsportal/WhxKECPNujWoWEFNdnJE.png',
+    link: '',
+  },
+];
+
+@connect(({ project, activities, chart, loading }) => ({
+  project,
+  activities,
+  chart,
+  projectLoading: loading.effects['project/fetchNotice'],
+  activitiesLoading: loading.effects['activities/fetchList'],
+}))
+export default class Workplace extends PureComponent {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'project/fetchNotice',
+    });
+    dispatch({
+      type: 'activities/fetchList',
+    });
+    dispatch({
+      type: 'chart/fetch',
+    });
+  }
+
+  componentWillUnmount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'chart/clear',
+    });
+  }
+
+  renderActivities() {
+    const {
+      activities: { list },
+    } = this.props;
+    return list.map(item => {
+      const events = item.template.split(/@\{([^{}]*)\}/gi).map(key => {
+        if (item[key]) {
+          return (
+            <a href={item[key].link} key={item[key].name}>
+              {item[key].name}
+            </a>
+          );
+        }
+        return key;
+      });
+      return (
+        <List.Item key={item.id}>
+          <List.Item.Meta
+            avatar={<Avatar src={item.user.avatar} />}
+            title={
+              <span>
+                <a className={styles.username}>{item.user.name}</a>
+                &nbsp;
+                <span className={styles.event}>{events}</span>
+              </span>
+            }
+            description={
+              <span className={styles.datetime} title={item.updatedAt}>
+                {moment(item.updatedAt).fromNow()}
+              </span>
+            }
+          />
+        </List.Item>
+      );
+    });
+  }
+
+  render() {
+    const {
+      project: { notice },
+      projectLoading,
+      activitiesLoading,
+      chart: { radarData },
+    } = this.props;
+
+    const pageHeaderContent = (
+      <div className={styles.pageHeaderContent}>
+        <div className={styles.avatar}>
+          <Avatar
+            size="large"
+            src=".././assets/bgy_logo.PNG"
+            style={{backgroundColor:'#87d068'}}
+          />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contentTitle}>机器人餐厅设备总览</div>
+          <div>AGV状态｜FANUC机器人状态 | UR机器人状态 </div>
+        </div>
+      </div>
+    );
+
+    const extraContent = (
+      <div className={styles.extraContent}>
+        <div className={styles.statItem}>
+          <p>设备数量</p>
+          <p>13</p>
+        </div>
+        <div className={styles.statItem}>
+          <p>正在运行</p>
+          <p>
+            8
+            <span> / 13</span>
+          </p>
+        </div>
+        <div className={styles.statItem}>
+          <p>总运行次数</p>
+          <p>2,223</p>
+        </div>
+      </div>
+    );
+
+    return (
+      <PageHeaderLayout content={pageHeaderContent} extraContent={extraContent}>
+        <Row gutter={24}>
+          <Col xl={16} lg={24} md={24} sm={24} xs={24}>
+            <Card
+              className={styles.projectList}
+              style={{ marginBottom: 24 }}
+              title="AGV机器人状态"
+              bordered={false}
+              extra={<Link to="/dashboard/workplace">全部项目</Link>}
+              loading={projectLoading}
+              bodyStyle={{ padding: 0 }}
+            >
+              {notice.map(item => (
+                <Card.Grid className={styles.projectGrid} key={item.id}>
+                  <Card bodyStyle={{ padding: 0 }} bordered={false}>
+                    <Card.Meta
+                      title={
+                        <div className={styles.cardTitle}>
+                          <img size="small" src={aler} alt="loading.." width="22px" />
+                          <a>{item.title}</a>
+                        </div>
+                      }
+                      description={
+                        <div>
+                          <p>{item.description[0]}</p>
+                          <p>{item.description[1]}</p>
+                          <p>{item.description[2]}</p>
+                        </div>
+                      }
+                    />
+                    <div className={styles.projectItemContent}>
+                      <Link to="/dashboard/workplace">{item.member || ''}</Link>
+                      {item.updatedAt && (
+                        <span className={styles.datetime} title={item.updatedAt}>
+                          {moment(item.updatedAt).fromNow()}
+                        </span>
+                      )}
+                    </div>
+                  </Card>
+                </Card.Grid>
+              ))}
+            </Card>
+            <Card
+              bodyStyle={{ padding: 0 }}
+              bordered={false}
+              className={styles.activeCard}
+              title="餐厅动态"
+              loading={activitiesLoading}
+            >
+              <List loading={activitiesLoading} size="large">
+                {/* <div className={styles.activitiesList}>{this.renderActivities()}</div> */}
+                <div className={styles.activitiesList}>这里展示餐厅设备的实时运转情况</div>
+              </List>
+            </Card>
+          </Col>
+          <Col xl={8} lg={24} md={24} sm={24} xs={24}>
+            <Card
+              style={{ marginBottom: 24 }}
+              title="UR机器人状态"
+              bordered={false}
+              bodyStyle={{ padding: 0 }}
+            >
+              <EditableLinkGroup onAdd={() => {}} links={links} linkElement={Link} />
+            </Card>
+            <Card
+              style={{ marginBottom: 24 }}
+              bordered={false}
+              title="机器人服务效率 指数"
+              loading={radarData.length === 0}
+            >
+              <div className={styles.chart}>
+                <Radar hasLegend height={343} data={radarData} />
+              </div>
+            </Card>
+            <Card bodyStyle={{ paddingTop: 12, paddingBottom: 12 }} bordered={false} title="团队">
+              <div className={styles.members}>
+                <Row gutter={48}>
+                  {members.map(item => (
+                    <Col span={12} key={`members-item-${item.id}`}>
+                      <Link to={item.link}>
+                        <Avatar src={item.logo} size="small" />
+                        <span className={styles.member}>{item.title}</span>
+                      </Link>
+                    </Col>
+                  ))}
+                </Row>
+              </div>
+            </Card>
+          </Col>
+        </Row>
+      </PageHeaderLayout>
+    );
+  }
+}
